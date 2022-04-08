6c0b9fe4ab90117ccd87a4f297713b28508dbd88
NathanGer <nathangerard.1997@gmail.com>
2022-04-08 21:44:30 +0200

    on continue

--- Boid Simulation/Assets/Scripts/FishBehaviour.cs
+++ Boid Simulation/Assets/Scripts/FishBehaviour.cs
@@ -4,14 +4,78 @@ using UnityEngine;
 
 public class FishBehaviour : MonoBehaviour
 {
-    public enum FishType
+    #region Attributs
+    private FishType CurrentFishType;
+    [SerializeField] private List<FishBehaviour> CurrentShoal;
+    
+    [Range(0,10)]
+    [SerializeField] private float CurrentFishSpeed;
+    
+    [Range(0,10)]
+    [SerializeField] private float RandomTurnSensitivity;
+    #endregion
+
+    #region Initialisation
+    public void InitializeFish(FishType fishType,ref List<FishBehaviour> fishShoal)
     {
-        Blue,
-        Shark
+        CurrentShoal = fishShoal;
+        CurrentFishType = fishType;
+    }
+    #endregion
+
+    #region Movement
+    private void RandomOrientation()
+    {
+        this.transform.localRotation = Quaternion.Euler(this.transform.localRotation.eulerAngles.x,
+                                                        this.transform.localRotation.eulerAngles.y+Random.Range(-RandomTurnSensitivity,RandomTurnSensitivity),
+                                                        this.transform.localRotation.eulerAngles.z);
     }
 
-    public void InitializeFish()
+    private void MooveForward()
     {
+        this.transform.position += this.transform.right * Time.deltaTime * CurrentFishSpeed;
+    }
+    #endregion
+
+    private void Update()
+    {
+        RandomOrientation();
+        MooveForward();
+        CheckOutOfTheBox();
+    }
+
+    private void CheckOutOfTheBox()
+    {
+        if(this.transform.position.x > 5)
+        {
+            this.transform.position -= Vector3.right * 10;
+        }
+        if(this.transform.position.x < -5)
+        {
+            this.transform.position += Vector3.right * 10;
+        }
+        if(this.transform.position.z > 5)
+        {
+            this.transform.position -= Vector3.forward * 10;
+        }        
+        if(this.transform.position.z < -5)
+        {
+            this.transform.position += Vector3.forward * 10;
+        }
+        if(this.transform.position.y > 5)
+        {
+            this.transform.position -= Vector3.up * 10;
+        }        
+        if(this.transform.position.y < -5)
+        {
+            this.transform.position += Vector3.up * 10;
+        }
 
     }
+}
+
+public enum FishType
+{
+    Blue,
+    Shark
 }
--- Boid Simulation/Assets/Scripts/FishFactory.cs
+++ Boid Simulation/Assets/Scripts/FishFactory.cs
@@ -6,15 +6,36 @@ public class FishFactory : MonoBehaviour
 {
     [Header("Choix du nombre de poisson par banc")]
     [Range(0, 100)]
-    [SerializeField] int NbrOfFish;
+    [SerializeField] int NbrOfBlueFish;
+    
+    [Header("Prefab des poissons")]
+    [SerializeField] GameObject BlueFish;
+
+    private List<FishBehaviour> BlueFishList = new List<FishBehaviour>();
 
     private void Awake()
     {
-        CreateFishShoal();
+        CreateBlueFishShoal();
     }
 
-    private void CreateFishShoal()
+    private void CreateBlueFishShoal()
     {
+        if (NbrOfBlueFish > 0)
+        {
+            for (int i = 0; i < NbrOfBlueFish; i++)
+            {
+                FishBehaviour newFish = InstantiateFish(BlueFish);
+                BlueFishList.Add(newFish);
+                newFish.InitializeFish(FishType.Blue, ref BlueFishList);                
+            }
+        }
+    }
 
+    private FishBehaviour InstantiateFish(GameObject FishPrefab)
+    {
+        GameObject newFish = Instantiate(BlueFish,
+                                            new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)),
+                                            Quaternion.Euler(0, Random.Range(-180,180), 0));
+        return newFish.GetComponent<FishBehaviour>();
     }
 }
