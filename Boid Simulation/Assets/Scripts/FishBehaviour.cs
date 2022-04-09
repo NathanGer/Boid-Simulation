38ebf8e235171354aa7e2b2b5eb89cf6cf2d0151
NathanGer <nathangerard.1997@gmail.com>
2022-04-09 21:52:11 +0200

    implem boid and shark

--- Boid Simulation/Assets/Scripts/FishBehaviour.cs
+++ Boid Simulation/Assets/Scripts/FishBehaviour.cs
@@ -5,72 +5,292 @@ using UnityEngine;
 public class FishBehaviour : MonoBehaviour
 {
     #region Attributs
-    private FishType CurrentFishType;
-    [SerializeField] private List<FishBehaviour> CurrentShoal;
-    
-    [Range(0,10)]
-    [SerializeField] private float CurrentFishSpeed;
-    
-    [Range(0,10)]
-    [SerializeField] private float RandomTurnSensitivity;
+    public FishType CurrentFishType { get; set; }
+    [SerializeField] private List<FishData> CurrentShoalData;
+    [SerializeField] private List<FishData> CurrentSharkData;
+
+    [Header("Speed ratio")]
+    [Range(0,10)] [SerializeField] private float MoovingFishSpeed ;
+    [Range(0,10)] [SerializeField] private float RotationSpeed = 1;
+    [Range(0,10)] [SerializeField] private float RandomTurnSensitivity;
+
+    [Header("Angle mort")]
+    [Range(0, 1)][SerializeField] private float DetectionAngle = 145f;
+
+    [Header("Distance ratio")]
+    [Range(0, 10)] [SerializeField] private float AlignmentDistance = 2f;
+    [Range(0, 10)] [SerializeField] private float ConvergenceDistance = 3f;
+    [Range(0, 10)] [SerializeField] private float RepulsionDistance = 1f;
+    [Range(0, 10)] [SerializeField] private float SharkRepulsionDistance = 8f;
+    [Range(0, 10)] [SerializeField] float BoundLimits = 4.5f;
+
+    [Header("Force ratio")]
+    [Range(0, 10)][SerializeField] private float AlignementForceRatio =1;
+    [Range(0, 10)][SerializeField] private float ConvergenceForceRatio =1;
+    [Range(0, 10)][SerializeField] private float BoundRepulsionForceRatio = 1;
+    [Range(0, 10)][SerializeField] private float RepulsionForceRatio = 1;
+    [Range(0, 10)][SerializeField] private float SharkRepulsionForceRatio = 1;
+
+    Vector3 ResultanteForce;
+
     #endregion
 
     #region Initialisation
-    public void InitializeFish(FishType fishType,ref List<FishBehaviour> fishShoal)
+    public void InitializeFish(FishType fishType,List<FishBehaviour> fishShoal,List<FishBehaviour>  sharkList = null)
     {
-        CurrentShoal = fishShoal;
-        CurrentFishType = fishType;
+        CurrentShoalData = new List<FishData>();
+        foreach (var fish in fishShoal)
+        {
+            if (fish != this)
+            {
+                FishData newFishData = new FishData(fish,
+                                                    Vector3.zero,
+                                                    0,
+                                                    0,
+                                                    fish.CurrentFishType);
+                CurrentShoalData.Add(newFishData);
+            }
+        }
+
+        CurrentSharkData = new List<FishData>();
+        if (sharkList != null)
+        {
+            foreach (var shark in sharkList)
+            {
+     
+                    FishData newSharkData = new FishData(shark,
+                                                        Vector3.zero,
+                                                        0,
+                                                        0,
+                                                        shark.CurrentFishType);
+                    CurrentSharkData.Add(newSharkData);  
+            }
+        }
+    }
+    #endregion
+
+    #region DataCalcul
+
+
+    private void CalculDistance(List<FishData> shoal)
+    {
+        for (int i = 0; i < shoal.Count; i++)
+        {
+            FishData data = new FishData(shoal[i]);
+            data.Distance = Vector3.Distance(this.transform.position, shoal[i].FishBehaviour.transform.position);
+            shoal[i] = data;
+        }
+    }
+
+    private void CalculDirection(List<FishData> shoal)
+    {
+        for (int i = 0; i < shoal.Count; i++)
+        {
+            FishData data = new FishData(shoal[i]);
+            data.DirectionToTheFish = (data.FishBehaviour.transform.position - this.transform.position).normalized;
+            shoal[i] = data;
+        }
+    }    
+    
+    private void CalculAngle(List<FishData> shoal)
+    {
+        for (int i = 0; i < shoal.Count; i++)
+        {
+            FishData data = new FishData(shoal[i]);
+            data.Angle = Vector3.Angle(this.transform.forward, data.DirectionToTheFish);
+            shoal[i] = data;
+        }
     }
     #endregion
 
     #region Movement
+    private void MooveForward(float magnitude = 1)
+    {
+        this.transform.position += this.transform.forward * Time.deltaTime * MoovingFishSpeed * magnitude;
+    }
+
     private void RandomOrientation()
     {
         this.transform.localRotation = Quaternion.Euler(this.transform.localRotation.eulerAngles.x,
-                                                        this.transform.localRotation.eulerAngles.y+Random.Range(-RandomTurnSensitivity,RandomTurnSensitivity),
-                                                        this.transform.localRotation.eulerAngles.z);
+                                                        this.transform.localRotation.eulerAngles.y + Random.Range(-RandomTurnSensitivity, RandomTurnSensitivity),
+                                                        this.transform.localRotation.eulerAngles.z + Random.Range(-RandomTurnSensitivity, RandomTurnSensitivity));
+    }
+    #endregion
+
+    #region ForceCalculation
+    private Vector3 CalculRepulsionForce(List<FishData> shoal, float distance)
+    {
+        Vector3 RepulsionForceSum = Vector3.zero;
+        if (shoal.Count > 0)
+        {
+            for (int i = 0; i < shoal.Count; i++)
+            {
+                if (shoal[i].Distance < distance)
+                {
+                    RepulsionForceSum += -shoal[i].DirectionToTheFish;
+                }
+            }
+        }
+        return RepulsionForceSum.normalized;
+    }
+    private Vector3 CalculAlignementForce(List<FishData> shoal, float distance)
+    {
+        Vector3 AlignementForceSum = Vector3.zero;
+        for (int i = 0; i < shoal.Count; i++)
+        {
+            if (shoal[i].Distance < distance && shoal[i].Distance>RepulsionDistance)
+            {
+                AlignementForceSum += shoal[i].FishBehaviour.transform.forward;
+            }
+        }
+        return AlignementForceSum.normalized;
+    }
+    private Vector3 CalculConvergenceForce(List<FishData> shoal, float distance)
+    {
+        Vector3 ConvergenceForceSum = Vector3.zero;
+        for (int i = 0; i < shoal.Count; i++)
+        {
+            if (shoal[i].Distance < distance && shoal[i].Distance > RepulsionDistance)
+            {
+                ConvergenceForceSum += shoal[i].DirectionToTheFish;
+            }
+        }
+        return ConvergenceForceSum.normalized;
+    }
+    private Vector3 CheckOutOfTheBox()
+    {
+        Vector3 force = Vector3.zero;
+        if (this.transform.position.x > BoundLimits)
+        {
+            force = Vector3.left * BoundRepulsionForceRatio;
+        }
+        if (this.transform.position.x < -BoundLimits)
+        {
+            force = Vector3.right * BoundRepulsionForceRatio;
+        }
+        if (this.transform.position.z > BoundLimits)
+        {
+            force = Vector3.back * BoundRepulsionForceRatio;
+        }
+        if (this.transform.position.z < -BoundLimits)
+        {
+            force = Vector3.forward * BoundRepulsionForceRatio;
+        }
+        if (this.transform.position.y > BoundLimits)
+        {
+            force = Vector3.down * BoundRepulsionForceRatio;
+        }
+        if (this.transform.position.y < -BoundLimits)
+        {
+            force = Vector3.up * BoundRepulsionForceRatio;
+        }
+        return force;
     }
 
-    private void MooveForward()
-    {
-        this.transform.position += this.transform.right * Time.deltaTime * CurrentFishSpeed;
-    }
     #endregion
 
     private void Update()
     {
-        RandomOrientation();
-        MooveForward();
-        CheckOutOfTheBox();
+        ResultanteForce = Vector3.zero;
+        if(CurrentShoalData != null)
+        {
+            CalculDirection(CurrentSharkData);
+            CalculAngle(CurrentSharkData);
+            CalculDistance(CurrentSharkData); 
+            
+            CalculDirection(CurrentShoalData);
+            CalculAngle(CurrentShoalData);
+            CalculDistance(CurrentShoalData);
+
+            ResultanteForce += RepulsionForceRatio * CalculRepulsionForce(CurrentShoalData,RepulsionDistance);
+            ResultanteForce += SharkRepulsionForceRatio * CalculRepulsionForce(CurrentSharkData, SharkRepulsionDistance);
+            ResultanteForce += AlignementForceRatio * CalculAlignementForce(CurrentShoalData,AlignmentDistance);
+            ResultanteForce += ConvergenceForceRatio * CalculConvergenceForce(CurrentShoalData,ConvergenceDistance);
+            ResultanteForce += BoundRepulsionForceRatio * CheckOutOfTheBox();
+
+            ApplyForce(ResultanteForce);
+            MooveForward();
+        }
+
     }
 
-    private void CheckOutOfTheBox()
+    private void ApplyForce(Vector3 ResultanteForce)
     {
-        if(this.transform.position.x > 5)
-        {
-            this.transform.position -= Vector3.right * 10;
-        }
-        if(this.transform.position.x < -5)
-        {
-            this.transform.position += Vector3.right * 10;
-        }
-        if(this.transform.position.z > 5)
-        {
-            this.transform.position -= Vector3.forward * 10;
-        }        
-        if(this.transform.position.z < -5)
-        {
-            this.transform.position += Vector3.forward * 10;
-        }
-        if(this.transform.position.y > 5)
-        {
-            this.transform.position -= Vector3.up * 10;
-        }        
-        if(this.transform.position.y < -5)
-        {
-            this.transform.position += Vector3.up * 10;
-        }
+        float AppliedNorme = ResultanteForce.magnitude;
+        Vector3 AppliedRotation = ResultanteForce.normalized;
+        float singleStep = RotationSpeed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, ResultanteForce, singleStep, 0.0f);
+        transform.LookAt(this.transform.position+ newDirection);
+    }
 
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0.5f, 0.5f, 0.5f, 0.05f);
+        //Gizmos.DrawSphere(this.transform.position, ConvergenceDistance);        
+        Gizmos.color = new Color(1f, 0, 0, 0.1f);
+        Gizmos.DrawSphere(this.transform.position, RepulsionDistance);
+        Gizmos.color = new Color(1.0f, 0.64f, 0.0f, 0.1f);
+        //Gizmos.DrawSphere(this.transform.position, AlignmentDistance);
+
+        for (int i = 0; i < CurrentShoalData.Count; i++)
+        {
+            if (CurrentShoalData[i].Angle < 145 && CurrentShoalData[i].Distance<RepulsionDistance)
+            {
+                Gizmos.color = Color.cyan ;
+            }
+            else if (CurrentShoalData[i].Angle < 145 && CurrentShoalData[i].Distance> RepulsionDistance)
+            {
+                //Gizmos.color = Color.green;
+                Gizmos.color = Color.clear;
+            }
+            else
+            {
+                Gizmos.color = Color.red;
+            }
+            if (CurrentShoalData[i].Distance <= ConvergenceDistance)
+            {
+                Gizmos.DrawLine(this.transform.position, this.transform.position + CurrentShoalData[i].DirectionToTheFish);
+            }
+            if (ResultanteForce != Vector3.zero)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(this.transform.position, this.transform.position + ResultanteForce);
+
+            }
+
+        }
+    }
+}
+
+public struct FishData
+{
+    public FishBehaviour FishBehaviour { get; set; }
+    public Vector3 DirectionToTheFish;
+    public float Distance { get; set; }
+    public float Angle;
+    public FishType FishType;
+
+    public FishData(FishBehaviour fishBehaviour,
+                    Vector3 directionToTheFish,
+                    float distance,
+                    float angle,
+                    FishType fishType)
+    {
+        FishBehaviour = fishBehaviour;
+        DirectionToTheFish = directionToTheFish;
+        Distance = distance;
+        Angle = angle;
+        FishType = fishType;
+    }
+
+    public FishData(FishData data)
+    {
+        FishBehaviour = data.FishBehaviour;
+        DirectionToTheFish = data.DirectionToTheFish;
+        Distance = data.Distance;
+        Angle = data.Angle;
+        FishType = data.FishType;
     }
 }
 
--- Boid Simulation/Assets/Scripts/FishFactory.cs
+++ Boid Simulation/Assets/Scripts/FishFactory.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class FishFactory : MonoBehaviour
 {
     [Header("Choix du nombre de poisson par banc")]
-    [Range(0, 100)]
+    [Range(0, 1000)]
     [SerializeField] int NbrOfBlueFish;
+    [Range(0, 10)]
+    [SerializeField] int NbrOfShark;
     
     [Header("Prefab des poissons")]
     [SerializeField] GameObject BlueFish;
+    [SerializeField] GameObject Shark;
 
     private List<FishBehaviour> BlueFishList = new List<FishBehaviour>();
+    private List<FishBehaviour> SharkList = new List<FishBehaviour>();
 
     private void Awake()
     {
+        CreateSharkShoal();
         CreateBlueFishShoal();
     }
 
@@ -26,16 +31,44 @@ public class FishFactory : MonoBehaviour
             {
                 FishBehaviour newFish = InstantiateFish(BlueFish);
                 BlueFishList.Add(newFish);
-                newFish.InitializeFish(FishType.Blue, ref BlueFishList);                
+                newFish.CurrentFishType = FishType.Blue;
+            }
+        }
+        InitializeFish(BlueFishList,SharkList);
+    }    
+    private void CreateSharkShoal()
+    {
+        if (NbrOfShark > 0)
+        {
+            for (int i = 0; i < NbrOfShark; i++)
+            {
+                FishBehaviour newFish = InstantiateFish(Shark);
+                SharkList.Add(newFish);
+                newFish.CurrentFishType = FishType.Shark;
+            }
+        }
+        InitializeFish(SharkList);
+    }
+
+    private void InitializeFish(List<FishBehaviour>  fishList, List<FishBehaviour>  sharkList = null)
+    {
+        foreach (var fish in fishList)
+        {
+            if(sharkList != null)
+            {
+                fish.InitializeFish(FishType.Blue, fishList, sharkList);
+            }
+            else
+            {
+                fish.InitializeFish(FishType.Blue, fishList);
             }
         }
     }
-
     private FishBehaviour InstantiateFish(GameObject FishPrefab)
     {
-        GameObject newFish = Instantiate(BlueFish,
-                                            new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)),
-                                            Quaternion.Euler(0, Random.Range(-180,180), 0));
+        GameObject newFish = Instantiate(FishPrefab,
+                                            new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)),
+                                            Quaternion.Euler(0, Random.Range(-180,180), Random.Range(-180, 180)));
         return newFish.GetComponent<FishBehaviour>();
     }
 }
